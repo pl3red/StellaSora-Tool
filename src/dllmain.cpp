6cc747b499947491101af3d74e51f27986243817
pl3red <a03telurd@gmail.com>
2025-11-29 10:22:58 +0700

    Update dllmain.cpp

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,17 +1,49 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 #include "main.h"
+#include <process.h>   // for _beginthreadex
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
-	switch (ul_reason_for_call) {
-		case DLL_PROCESS_ATTACH:
-			CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Run, new HMODULE(hModule), 0, NULL);
-			break;
-		case DLL_THREAD_ATTACH:
-		case DLL_THREAD_DETACH:
-		case DLL_PROCESS_DETACH:
-			break;
-	}
-    return TRUE;
+// Wrapper thread for safety
+unsigned __stdcall ThreadEntry(void* param)
+{
+    HMODULE hModule = (HMODULE)param;
+
+    // Start your mod
+    Run(hModule);
+
+    return 0;
 }
 
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        DisableThreadLibraryCalls(hModule);
+        
+        // Use _beginthreadex (SAFE inside DllMain)
+        {
+            uintptr_t thread = _beginthreadex(
+                nullptr,        // default security
+                0,              // default stack
+                ThreadEntry,    // entry function
+                hModule,        // pass REAL module handle
+                0,
+                nullptr
+            );
+
+            if (thread)
+                CloseHandle((HANDLE)thread);
+        }
+        break;
+
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+        // should never arrive because of DisableThreadLibraryCalls
+        break;
+
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+
+    return TRUE;
+}
